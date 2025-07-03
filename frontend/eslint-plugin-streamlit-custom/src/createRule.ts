cb7588d3873f4a2164c4c119d1c83168b6003156
Bob Nisco <bob.nisco@snowflake.com>
2025-07-03 11:37:02 -0700

    Migrate everything to typescript

--- frontend/eslint-plugin-streamlit-custom/src/createRule.ts
+++ frontend/eslint-plugin-streamlit-custom/src/createRule.ts
@@ -14,14 +14,10 @@
  * limitations under the License.
  */
 
-const useStrictNullEqualityChecks = require("./use-strict-null-equality-checks")
-const noHardcodedThemeValues = require("./no-hardcoded-theme-values")
-const enforceMemo = require("./enforce-memo")
+import { ESLintUtils } from "@typescript-eslint/utils"
 
-module.exports = {
-  rules: {
-    "use-strict-null-equality-checks": useStrictNullEqualityChecks,
-    "no-hardcoded-theme-values": noHardcodedThemeValues,
-    "enforce-memo": enforceMemo,
-  },
-}
+// Create a rule creator for our custom ESLint rules
+export const createRule = ESLintUtils.RuleCreator(
+  name =>
+    `https://github.com/streamlit/streamlit/blob/develop/frontend/eslint-plugin-streamlit-custom/src/${name}.ts`
+)
